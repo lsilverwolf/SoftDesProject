c24078b8d70b465be9c0fcb5f834372415f55227
Sharon <sharon.grimshaw@students.olin.edu>
2013-04-22 13:57:50 -0400

    "I changed tkinter_text.py to have the cal button"

--- tkinter_text.py
+++ tkinter_text.py
@@ -1,10 +1,26 @@
 from tkinter import *
+import dlgCalendar2 as dlgCal
+import time
 
-master = Tk()
+year = time.localtime()[0]
+month = time.localtime()[1]
+day = time.localtime()[2]
+strdate = (str(month) +  "/" + str(day) + "/" + str(year))
 
 def close_window():
-    # Destroying your main window (root)
     master.destroy()
+
+def fnCalendar():
+    d1 = dlgCal.tkCalendar(master, year, month, day, StringVar())
+
+
+master = Tk()
+v1 = IntVar()
+v2 = IntVar()
+v3 = IntVar()
+v4 = IntVar()
+v5 = IntVar()
+master.title("New Task")
 
 Label(master,text="Enter Task:").grid(row=0,sticky=W,)
 Label(master,text="Enter Due Date:").grid(row=1,sticky=W)
@@ -12,18 +28,29 @@ Label(master,text="Priority:").grid(row=2,sticky=W)
 Label(master,text="Requested Start Date:").grid(row=3,sticky=W)
 Label(master,text="Requested End Date:").grid(row=4,sticky=W)
 
-
+Radiobutton(master,text=str(1),variable=v1,value=1).grid(row=2,column=1)
+Radiobutton(master,text=str(2),variable=v1,value=2).grid(row=2,column=2)
+Radiobutton(master,text=str(3),variable=v1,value=3).grid(row=2,column=3)
+Radiobutton(master,text=str(4),variable=v1,value=4).grid(row=2,column=4)
+Radiobutton(master,text=str(5),variable=v1,value=5).grid(row=2,column=5)
 
 e1 = Entry(master)
 e2 = Entry(master)
 e3 = Entry(master)
 e4 = Entry(master)
 
-e1.grid(row=0,column=1)
-e2.grid(row=1,column=1)
-e3.grid(row=2,column=1)
-e4.grid(row=2,column=3)
+e1.grid(row=0,column=1,columnspan=5)
+e2.grid(row=1,column=1,columnspan=5)
+e3.grid(row=3,column=1,columnspan=5)
+e4.grid(row=4,column=1,columnspan=5)
+height = 13
+width = 13
 
-b1 = Button(master,text="Submit",command=close_window).grid(row=3,sticky=E,column=3,padx=30)
+photo = PhotoImage(file = "cal_icon.gif")
+cal1 = Button(master,height=height,width=width, image = photo,command=fnCalendar).grid(row=3,column=6,sticky=W,padx=10)
+cal2 = Button(master,height=height,width=width, image = photo,command=fnCalendar).grid(row=1,column=6,sticky=W,padx=10)
+cal3 = Button(master,height=height,width=width, image = photo,command=fnCalendar).grid(row=4,column=6,sticky=W,padx=10)
+
+b1 = Button(master,text="Submit",command=close_window).grid(row=5,sticky=E,column=4,padx=10)
 
 mainloop()
